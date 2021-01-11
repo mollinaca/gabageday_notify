6b5f7d7161575aad501a49c382181354b6f987c5
mollinaca <dev@m.rel0.pw>
2021-01-11 10:39:28 +0900

    modify クォートを統一

--- flayers.py
+++ flayers.py
@@ -18,14 +18,14 @@ def files_upload (token:str, channel:str, f:str, comment:str):
     """
     Slackチャンネルへファイルをアップロードする
     """
-    url = "https://slack.com/api/files.upload"
+    url = 'https://slack.com/api/files.upload'
     files = {'file': open(f, 'rb')}
     data = {
         'token': token,
         'channels': channel,
         'filename': f,
         'initial_comment': comment,
-        'filetype': "jpg"
+        'filetype': 'jpg'
     }
     res = requests.post(url, data=data, files=files)
     return res
@@ -35,7 +35,7 @@ def iw (webhook:str, message:str):
     SlackチャンネルへIncomingWebhookを使ってメッセージをポストする
     """
     data = json.dumps({
-    "text" : message
+    'text' : message
     })
     res = requests.post(webhook, data)
     return res
@@ -50,7 +50,7 @@ def prev_flayer () -> dict:
     return res
 
 def dl (url:str, title:str) -> str:
-    urllib.request.urlretrieve(url,"{0}".format(title))
+    urllib.request.urlretrieve(url,'{0}'.format(title))
     return title
 
 def york () -> dict:
@@ -60,15 +60,15 @@ def york () -> dict:
     dt_now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
 
     # 最新のチラシのファイル名を取得
-    york_url = "https://www.york-inc.com"
-    load_url = york_url + "/store/%e5%a4%a7%e5%ae%ae%e5%8d%97%e4%b8%ad%e9%87%8e%e5%ba%97.html"
+    york_url = 'https://www.york-inc.com'
+    load_url = york_url + '/store/%e5%a4%a7%e5%ae%ae%e5%8d%97%e4%b8%ad%e9%87%8e%e5%ba%97.html'
     res = requests.get(load_url)
-    html = BeautifulSoup(res.content, "html.parser")
+    html = BeautifulSoup(res.content, 'html.parser')
     leaflet = html.find_all(class_='leaflet pc')
     yorkmart_flayers = []
     for l in leaflet:
         for l2 in list(l.findAll('img')[0].get('data-set').split(',')):
-            if "ul-files" in l2:
+            if 'ul-files' in l2:
                 yorkmart_flayers.append(york_url + l2)
 
     ret = {'updated_at':dt_now, 'flayers':yorkmart_flayers}
@@ -83,18 +83,18 @@ def meatmeet () -> dict:
     # 最新のチラシのファイル名を取得
 
     # チラシページURLを取得
-    tokubai_url = "https://tokubai.co.jp"
-    meatmeet_url = tokubai_url + "/MEATMeet%E9%A3%9F%E8%82%89%E5%8D%B8%E5%A3%B2%E3%82%BB%E3%83%B3%E3%82%BF%E3%83%BC/174289"
+    tokubai_url = 'https://tokubai.co.jp'
+    meatmeet_url = tokubai_url + '/MEATMeet%E9%A3%9F%E8%82%89%E5%8D%B8%E5%A3%B2%E3%82%BB%E3%83%B3%E3%82%BF%E3%83%BC/174289'
     req = urllib.request.Request(meatmeet_url)
     with urllib.request.urlopen(req) as res:
         body = res.read()
-    html = BeautifulSoup(body, "html.parser")
-    leaflet_page_url = tokubai_url + html.findAll(class_="leaflet_component")[0].find(class_="image_element").get('href')
+    html = BeautifulSoup(body, 'html.parser')
+    leaflet_page_url = tokubai_url + html.findAll(class_='leaflet_component')[0].find(class_='image_element').get('href')
     req = urllib.request.Request(leaflet_page_url)
     with urllib.request.urlopen(req) as res:
         body = res.read()
-    html = BeautifulSoup(body, "html.parser")
-    leaflet_page_link = html.find_all(class_="other_leaflet_link")
+    html = BeautifulSoup(body, 'html.parser')
+    leaflet_page_link = html.find_all(class_='other_leaflet_link')
 
     # チラシページリンクのチラシページリンクURL（複数）を取得
     leaflet_page_links = []
@@ -108,8 +108,8 @@ def meatmeet () -> dict:
         with urllib.request.urlopen(req) as res:
             body = res.read()
 
-        html = BeautifulSoup(body, "html.parser")
-        leaflet_links.append(html.find(class_='leaflet').get('src').split("?")[0])
+        html = BeautifulSoup(body, 'html.parser')
+        leaflet_links.append(html.find(class_='leaflet').get('src').split('?')[0])
 
     ret = {'updated_at':dt_now, 'flayers':leaflet_links}
     return ret
@@ -123,18 +123,18 @@ def gyomusuper () -> dict:
     # 最新のチラシのファイル名を取得
 
     # チラシページURLを取得
-    tokubai_url = "https://tokubai.co.jp"
-    gs_url = tokubai_url + "/%E6%A5%AD%E5%8B%99%E3%82%B9%E3%83%BC%E3%83%91%E3%83%BC/169887"
+    tokubai_url = 'https://tokubai.co.jp'
+    gs_url = tokubai_url + '/%E6%A5%AD%E5%8B%99%E3%82%B9%E3%83%BC%E3%83%91%E3%83%BC/169887'
     req = urllib.request.Request(gs_url)
     with urllib.request.urlopen(req) as res:
         body = res.read()
-    html = BeautifulSoup(body, "html.parser")
-    leaflet_page_url = tokubai_url + html.findAll(class_="leaflet_component")[0].find(class_="image_element").get('href')
+    html = BeautifulSoup(body, 'html.parser')
+    leaflet_page_url = tokubai_url + html.findAll(class_='leaflet_component')[0].find(class_='image_element').get('href')
     req = urllib.request.Request(leaflet_page_url)
     with urllib.request.urlopen(req) as res:
         body = res.read()
-    html = BeautifulSoup(body, "html.parser")
-    leaflet_page_link = html.find_all(class_="other_leaflet_link")
+    html = BeautifulSoup(body, 'html.parser')
+    leaflet_page_link = html.find_all(class_='other_leaflet_link')
 
     # チラシページリンクのチラシページリンクURL（複数）を取得
     leaflet_page_links = []
@@ -148,8 +148,8 @@ def gyomusuper () -> dict:
         with urllib.request.urlopen(req) as res:
             body = res.read()
 
-        html = BeautifulSoup(body, "html.parser")
-        leaflet_links.append(html.find(class_='leaflet').get('src').split("?")[0])
+        html = BeautifulSoup(body, 'html.parser')
+        leaflet_links.append(html.find(class_='leaflet').get('src').split('?')[0])
 
     ret = {'updated_at':dt_now, 'flayers':leaflet_links}
     return ret
@@ -168,12 +168,12 @@ def main():
 
     try:
         # 動作確認用
-        m = "[動作確認用] flayers.py を実行しました dt: " + dt_now
+        m = '[動作確認用] flayers.py を実行しました dt: ' + dt_now
         iw (webhook_dev, m)
         # 動作確認用ここまで
 
         SCRIPT_DIR = pathlib.Path(__file__).resolve().parent
-        OUTPUT_DIR = pathlib.Path(str(SCRIPT_DIR) + "/docs/flayer/")
+        OUTPUT_DIR = pathlib.Path(str(SCRIPT_DIR) + '/docs/flayer/')
         stores = ['yorkmart', 'meatmeet', 'gyomusuper',  'supervalue']
         isNew = False
 
@@ -186,12 +186,12 @@ def main():
                 print (store)
                 y = york ()
                 if set(y['flayers']) == set(pf['detail']['yorkmart']['flayers']):
-                    print (" -> flayers not renewed")
-                    text = "[動作確認用] " + store + " のチラシは更新されていませんでいした"
+                    print (' -> flayers not renewed')
+                    text = '[動作確認用] ' + store + ' のチラシは更新されていませんでいした'
                     iw (webhook_dev, text)
                 else:
-                    print (" -> got new flayers!")
-                    text = "ヨークマート の新しいチラシを取得しました！"
+                    print (' -> got new flayers!')
+                    text = 'ヨークマート の新しいチラシを取得しました！'
                     iw (webhook, text)
                     iw (webhook_dev, text)
                     pf['detail']['yorkmart'] = y
@@ -199,7 +199,7 @@ def main():
 
                     # img ファイルを取得
                     for flayer_url in y['flayers']:
-                        filename = flayer_url.split("/")[-1]
+                        filename = flayer_url.split('/')[-1]
                         comment = flayer_url
                         dl (flayer_url, filename)
 
@@ -211,7 +211,7 @@ def main():
                             ret = files_upload (token, channel, filename, comment)
                             #ret = files_upload (token, channel_dev, filename, comment)
                             if not ret.status_code == 200:
-                                print ("[error] requests response not <200 OK> ->", ret.headers['status'], filename, file=sys.stderr)
+                                print ('[error] requests response not <200 OK> ->', ret.headers['status'], filename, file=sys.stderr)
                         else:
                             pass
 
@@ -222,12 +222,12 @@ def main():
                 print (store)
                 m = meatmeet ()
                 if ('meatmeet' in pf['detail']) and set(m['flayers']) == set(pf['detail']['meatmeet']['flayers']):
-                    print (" -> flayers not renewed")
-                    text = "[動作確認用] " + store + " のチラシは更新されていませんでいした"
+                    print (' -> flayers not renewed')
+                    text = '[動作確認用] ' + store + ' のチラシは更新されていませんでいした'
                     iw (webhook_dev, text)
                 else:
-                    print (" -> got new flayers!")
-                    text = "ミートミート木崎 の新しいチラシを取得しました！"
+                    print (' -> got new flayers!')
+                    text = 'ミートミート木崎 の新しいチラシを取得しました！'
                     iw (webhook, text)
                     iw (webhook_dev, text)
                     pf['detail']['meatmeet'] = m
@@ -235,7 +235,7 @@ def main():
 
                     # img ファイルを取得
                     for flayer_url in m['flayers']:
-                        filename = flayer_url.split("/")[-1]
+                        filename = flayer_url.split('/')[-1]
                         comment = flayer_url
                         dl (flayer_url, filename)
 
@@ -247,7 +247,7 @@ def main():
                             ret = files_upload (token, channel, filename, comment)
                             #ret = files_upload (token, channel_dev, filename, comment)
                             if not ret.status_code == 200:
-                                print ("[error] requests response not <200 OK> ->", ret.headers['status'], filename, file=sys.stderr)
+                                print ('[error] requests response not <200 OK> ->', ret.headers['status'], filename, file=sys.stderr)
                         else:
                             pass
 
@@ -257,16 +257,16 @@ def main():
             elif store == 'supervalue': # スーパーバリュー
                 pass
 
-            elif store == "gyomusuper" : # 業務スーパー
+            elif store == 'gyomusuper' : # 業務スーパー
                 print (store)
                 gs = gyomusuper ()
                 if ('gyomusuper' in pf['detail']) and set(gs['flayers']) == set(pf['detail']['gyomusuper']['flayers']):
-                    print (" -> flayers not renewed")
-                    text = "[動作確認用] " + store + " のチラシは更新されていませんでいした"
+                    print (' -> flayers not renewed')
+                    text = '[動作確認用] ' + store + ' のチラシは更新されていませんでいした'
                     iw (webhook_dev, text)
                 else:
-                    print (" -> got new flayers!")
-                    text = "業務スーパー の新しいチラシを取得しました！"
+                    print (' -> got new flayers!')
+                    text = '業務スーパー の新しいチラシを取得しました！'
                     iw (webhook, text)
                     iw (webhook_dev, text)
                     pf['detail']['gyomusuper'] = gs
@@ -274,7 +274,7 @@ def main():
 
                     # img ファイルを取得
                     for flayer_url in gs['flayers']:
-                        filename = flayer_url.split("/")[-1]
+                        filename = flayer_url.split('/')[-1]
                         comment = flayer_url
                         dl (flayer_url, filename)
 
@@ -286,7 +286,7 @@ def main():
                             ret = files_upload (token, channel, filename, comment)
                             #ret = files_upload (token, channel_dev, filename, comment)
                             if not ret.status_code == 200:
-                                print ("[error] requests response not <200 OK> ->", ret.headers['status'], filename, file=sys.stderr)
+                                print ('[error] requests response not <200 OK> ->', ret.headers['status'], filename, file=sys.stderr)
                         else:
                             pass
 
@@ -299,8 +299,8 @@ def main():
         if isNew:
             # ファイルを更新
             pf['updated_at'] = dt_now
-            OUTPUT_FILE_NAME = "latest.json"
-            OUTPUT_FILE = pathlib.Path(str(OUTPUT_DIR) + "/" + OUTPUT_FILE_NAME)
+            OUTPUT_FILE_NAME = 'latest.json'
+            OUTPUT_FILE = pathlib.Path(str(OUTPUT_DIR) + '/' + OUTPUT_FILE_NAME)
 
             with open(OUTPUT_FILE, mode='w') as f:
                 f.write(json.dumps(pf, indent=4))
@@ -308,13 +308,13 @@ def main():
             # git 
             git_repo= git.Repo(SCRIPT_DIR)
             git_repo.index.add(str(OUTPUT_FILE))
-            commit_message = "[auto] update " + str(OUTPUT_FILE_NAME)
+            commit_message = '[auto] update ' + str(OUTPUT_FILE_NAME)
             git_repo.index.commit(commit_message)
             git_repo.remotes.origin.push('HEAD')
 
     except Exception as e:
-        err_msg = "```" + "[Exception]\n" + str(e) + "\n" + "[StackTrace]" + "\n" + traceback.format_exc() + "```"
+        err_msg = '```' + '[Exception]\n' + str(e) + '\n' + '[StackTrace]' + '\n' + traceback.format_exc() + '```'
         iw (webhook_dev, err_msg)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main ()
